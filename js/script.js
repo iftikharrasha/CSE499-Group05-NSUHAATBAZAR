e513f9c0434a4937cd3043257ad912b881e9a601
Iftikhar Rasha <iftikhar.rasha@northsouth.edu>
2020-11-30 21:55:23 +0600

    JQ plugins added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
+/********* sticky header *******/
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 0) {
+        $(".header").addClass("sticky");
+        $(".header-cover").addClass("sticky-none");
+        $(".logo").addClass("sticky-none");
+        $(".navbar-nav").addClass("mnew-auto");
+    } else {
+        $(".header").removeClass("sticky");
+        $(".header-cover").removeClass("sticky-none");
+        $(".logo").removeClass("sticky-none");
+        $(".navbar-nav").removeClass("mnew-auto");
+    }
+});
+/********* sticky header *******/
 
+/*********** wow js ************/
+$(document).ready(function () {
+    var wow = new WOW(
+        {
+            boxClass: 'wow',
+            animateClass: 'animated',
+            offset: 0,
+            mobile: true,
+            live: true,
+            callback: function (box) {
+            },
+            scrollContainer: null,
+            resetAnimation: true,
+        }
+    );
+    wow.init();
+});
+/*********** wow js ************/
